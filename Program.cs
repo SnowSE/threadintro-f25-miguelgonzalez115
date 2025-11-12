83f748170626367b8970c64c7fd453abca1b6c72
Miguel <miguel.gonzalez@students.snow.edu>
2025-11-11 22:06:47 -0700

    Done?

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
 ﻿namespace inclassLottery;
 
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System;
+
 class Ticket
 {
     public int[] RegTickets { get; set; }
@@ -7,11 +13,8 @@ class Ticket
     public Ticket(int[] numbers, int powerBall)
     {
         RegTickets = new int[5];
-        RegTickets[0] = numbers[0];
-        RegTickets[1] = numbers[1];
-        RegTickets[2] = numbers[2];
-        RegTickets[3] = numbers[3];
-        RegTickets[4] = numbers[4];
+        for (int i = 0; i < 5; i++)
+            RegTickets[i] = numbers[i];
         PowerBall = powerBall;
     }
     public Ticket()
@@ -22,6 +25,28 @@ class Ticket
             RegTickets[i] = Random.Shared.Next(1, 70);
         }
         PowerBall = Random.Shared.Next(1, 27);
+    }
+    public int GetMatchLevel(Ticket winning)
+    {
+        int regularMatches = 0;
+        foreach (int num in RegTickets)
+        {
+            if (winning.RegTickets.Contains(num))
+                regularMatches++;
+        }
+
+        bool powerballMatch = (PowerBall == winning.PowerBall);
+
+        if (regularMatches == 5 && powerballMatch) return 1; // power ball
+        if (regularMatches == 5) return 2;
+        if (regularMatches == 4 && powerballMatch) return 3;
+        if (regularMatches == 4) return 4;
+        if (regularMatches == 3 && powerballMatch) return 5;
+        if (regularMatches == 3) return 6;
+        if (regularMatches == 2 && powerballMatch) return 7;
+        if (regularMatches == 1 && powerballMatch) return 8;
+        if (powerballMatch) return 9;
+        return 0; // if no win
     }
 }
 class LotteryPeriod
@@ -38,6 +63,34 @@ class LotteryPeriod
     {
         WinningTicket = new Ticket(numbers, powerBall);
     }
+    public Dictionary<int, int> GatherStatistics()
+    {
+        Dictionary<int, int> stats = new Dictionary<int, int>();
+
+        foreach (var ticket in SoldTickets)
+        {
+            int level = ticket.GetMatchLevel(WinningTicket);
+            if (!stats.ContainsKey(level))
+                stats[level] = 0;
+            stats[level]++;
+        }
+
+        return stats;
+
+    }
+    public Dictionary<int, int> GatherStatisticsParallel()
+    {
+        var stats = new ConcurrentDictionary<int, int>();
+
+        Parallel.ForEach(SoldTickets, ticket =>
+        {
+            int level = ticket.GetMatchLevel(WinningTicket);
+            stats.AddOrUpdate(level, 1, (_, old) => old + 1);
+        });
+
+        return new Dictionary<int, int>(stats);
+    }
+
 }
 class LotteryVendor
 {
@@ -49,7 +102,10 @@ class LotteryVendor
         for (int i = 0; i < numberOfTickets; i++)
         {
             Ticket ticket = new Ticket();
-            period.SoldTickets.Add(ticket);
+            lock (period.SoldTickets)
+            {
+                period.SoldTickets.Add(ticket);
+            }
         }
     }
 }
@@ -60,9 +116,47 @@ class Program
     {
         Console.WriteLine("Hello, Lets sell 1Million Tickets!");
         LotteryPeriod period = new LotteryPeriod();
-        LotteryVendor vendor = new LotteryVendor();
-        vendor.SellTickets(period, 1_000_000);
-        Console.WriteLine("SOLD 1Million Tickets!");
+        //LotteryVendor vendor = new LotteryVendor();
+        //vendor.SellTickets(period, 1_000_000);
+        //Console.WriteLine("SOLD 1Million Tickets!");
+
+
+        LotteryVendor v1 = new LotteryVendor();
+        LotteryVendor v2 = new LotteryVendor();
+        LotteryVendor v3 = new LotteryVendor();
+
+        Thread t1 = new Thread(() => v1.SellTickets(period, 10_000_000));
+        Thread t2 = new Thread(() => v2.SellTickets(period, 10_000_000));
+        Thread t3 = new Thread(() => v3.SellTickets(period, 10_000_000));
+
+        var startTime = DateTime.Now;
+
+        t1.Start();
+        t2.Start();
+        t3.Start();
+
+        t1.Join();
+        t2.Join();
+        t3.Join();
+
+        var duration = DateTime.Now - startTime;
+
+        Console.WriteLine($"Total Tickets Sold: {period.SoldTickets.Count}");
+
+        
+        Console.WriteLine($"Total Tickets Sold: {period.SoldTickets.Count:N0}");
+        Console.WriteLine($"Time taken: {duration.TotalSeconds:F2} seconds\n");
+
+        Console.WriteLine("Gathering statistics...");
+        var stats = period.GatherStatisticsParallel(); // using parallel version
+
+        Console.WriteLine("\n=== Lottery Results ===");
+        foreach (var kv in stats.OrderBy(k => k.Key))
+        {
+            Console.WriteLine($"Level {kv.Key}: {kv.Value:N0} winners");
+        }
+
+        Console.WriteLine("\nSimulation complete!");
 
         //TODO: 1a) make 3 vendors sell 10M tickets each
         // 1b) 3 vendors sell tickets in parallel
@@ -70,5 +164,6 @@ class Program
         // 3) Gather statistics on how many winners of each level there are
         // 4) Print out the statistics
         // AFTER 1-4 is working, try to do (GatherStatistics) with Parallel Programming
+
     }
 }
